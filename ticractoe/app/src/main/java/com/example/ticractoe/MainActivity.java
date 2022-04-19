bc180163c66c6546165b608114e53d486cb2e2b1
HubertJ97 <84916127+HubertJ97@users.noreply.github.com>
2022-04-19 11:52:47 +0200

    first commit

---
+++ ticractoe/app/src/main/java/com/example/ticractoe/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.ticractoe;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start);
+    }
+
+    public void przejscie(View view) {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+    }
+
+    public void przejscie4(View view) {
+        Intent intent2 = new Intent(this, Game4Activity.class);
+        startActivity(intent2);
+    }
+    public void przejscie5(View view) {
+        Intent intent3 = new Intent(this, Game5Activity.class);
+        startActivity(intent3);
+    }
+
+
+}
